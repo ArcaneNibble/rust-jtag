b1b94f98eb724d1459083d426671a627552542f2
R <rqou@berkeley.edu>
2022-01-17 08:46:11 -0800

    sketch outline of ideas

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,318 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+pub enum JTAGState {
+    TestLogicReset,
+    RunTestIdle,
+
+    SelectDR,
+    CaptureDR,
+    ShiftDR,
+    Exit1DR,
+    PauseDR,
+    Exit2DR,
+    UpdateDR,
+
+    SelectIR,
+    CaptureIR,
+    ShiftIR,
+    Exit1IR,
+    PauseIR,
+    Exit2IR,
+    UpdateIR,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub enum JTAGAction {
+    /// Wait for a given number of nanoseconds
+    DelayNS(u64),
+    /// Set the JTAG clock speed in Hz
+    SetClkSpeed(u64),
+
+    /// Send 5 TMS=1 transitions, bringing the TAP to Test-Logic-Reset starting
+    /// from any state
+    ResetToTLR,
+    /// Transition the TAP through the specified states via the shortest
+    /// possible path
+    GoViaStates(Vec<JTAGState>),
+
+    /// Shift [bits_tdi][Self::ShiftBits::bits_tdi] into the TDI pin.
+    /// If [capture][Self::ShiftBits::capture] is `true`, TDO data will be
+    /// captured. If [tms_exit][Self::ShiftBits::tms_exit] is `true`,
+    /// the final bit will be shifted with TMS=1 (which would cause a
+    /// transition from Shift-IR/DR to Exit1-IR/DR). Otherwise, the final bit
+    /// will be shifted with TMS=0.
+    ShiftBits {
+        bits_tdi: Vec<bool>,
+        capture: bool,
+        tms_exit: bool,
+    },
+
+    /// Take the TAP from its current state to Shift-IR, then shift in the
+    /// data from [ir][Self::ShiftIR::ir] while capturing data if
+    /// [capture][Self::ShiftIR::capture] is `true`.
+    /// If [pause][Self::ShiftIR::pause] is `false`, then take the TAP to
+    /// Run-Test/Idle. Otherwise, take the TAP to Pause-IR.
+    ShiftIR {
+        ir: Vec<bool>,
+        capture: bool,
+        pause: bool,
+    },
+    /// Take the TAP from its current state to Shift-DR, then shift in the
+    /// data from [dr][Self::ShiftDR::dr] while capturing data if
+    /// [capture][Self::ShiftDR::capture] is `true`.
+    /// If [pause][Self::ShiftDR::pause] is `false`, then take the TAP to
+    /// Run-Test/Idle. Otherwise, take the TAP to Pause-DR.
+    ShiftDR {
+        dr: Vec<bool>,
+        capture: bool,
+        pause: bool,
+    },
+
+    /// If the current IR value is not equal to this value, take the TAP to
+    /// Shift-IR, shift in this value, and then take the TAP to Run-Test/Idle.
+    /// If the current IR value is equal, do nothing.
+    SetIR(Vec<bool>),
+    /// Set the current IR to this value if it is not already, and then
+    /// take the TAP to Shift-DR. Capture the data, and then take the TAP to
+    /// Run-Test/Idle.
+    ReadReg(Vec<bool>),
+    /// Set the current IR to [ir][Self::WriteReg::ir] if it is not already,
+    /// and then take the TAP to Shift-DR. Shift in the data from
+    /// [dr][Self::WriteReg::dr], and then take the TAP to Run-Test/Idle.
+    WriteReg { ir: Vec<bool>, dr: Vec<bool> },
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub enum JTAGOutput {
+    /// No data was stored for the corresponding JTAG action
+    NoData,
+    /// Captured TDO data for the corresponding JTAG action
+    CapturedBits(Vec<bool>),
+}
+
+#[derive(Clone, Debug)]
+pub struct JTAGAdapterState {
+    queued_actions: Vec<JTAGAction>,
+}
+impl JTAGAdapterState {
+    pub fn new() -> Self {
+        Self {
+            queued_actions: Vec::new(),
+        }
     }
 }
+
+pub trait JTAGAdapter: AsMut<JTAGAdapterState> {
+    fn execute_actions(&mut self, actions: &[JTAGAction]) -> Vec<JTAGOutput>;
+
+    fn flush(&mut self) -> Vec<JTAGOutput> {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        let mut actions = state.queued_actions.split_off(0);
+        self.execute_actions(&mut actions)
+    }
+    fn queue_action(&mut self, action: JTAGAction) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(action);
+    }
+
+    fn reset_to_tlr(&mut self) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ResetToTLR);
+    }
+    fn go_via_states(&mut self, states: &[JTAGState]) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state
+            .queued_actions
+            .push(JTAGAction::GoViaStates(states.to_vec()));
+    }
+
+    fn go_to_state(&mut self, state: JTAGState) {
+        self.go_via_states(&[state]);
+    }
+    fn go_tlr(&mut self) {
+        self.go_to_state(JTAGState::TestLogicReset);
+    }
+    fn go_rti(&mut self) {
+        self.go_to_state(JTAGState::RunTestIdle);
+    }
+    fn go_selectdr(&mut self) {
+        self.go_to_state(JTAGState::SelectDR);
+    }
+    fn go_capturedr(&mut self) {
+        self.go_to_state(JTAGState::CaptureDR);
+    }
+    fn go_shiftdr(&mut self) {
+        self.go_to_state(JTAGState::ShiftDR);
+    }
+    fn go_exit1dr(&mut self) {
+        self.go_to_state(JTAGState::Exit1DR);
+    }
+    fn go_pausedr(&mut self) {
+        self.go_to_state(JTAGState::PauseDR);
+    }
+    fn go_exit2dr(&mut self) {
+        self.go_to_state(JTAGState::Exit2DR);
+    }
+    fn go_updatedr(&mut self) {
+        self.go_to_state(JTAGState::UpdateDR);
+    }
+    fn go_selectir(&mut self) {
+        self.go_to_state(JTAGState::SelectIR);
+    }
+    fn go_captureir(&mut self) {
+        self.go_to_state(JTAGState::CaptureIR);
+    }
+    fn go_shiftir(&mut self) {
+        self.go_to_state(JTAGState::ShiftIR);
+    }
+    fn go_exit1ir(&mut self) {
+        self.go_to_state(JTAGState::Exit1IR);
+    }
+    fn go_pauseir(&mut self) {
+        self.go_to_state(JTAGState::PauseIR);
+    }
+    fn go_exit2ir(&mut self) {
+        self.go_to_state(JTAGState::Exit2IR);
+    }
+    fn go_updateir(&mut self) {
+        self.go_to_state(JTAGState::UpdateIR);
+    }
+
+    fn shift_bits_out(&mut self, bits: &[bool], tms_exit: bool) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftBits {
+            bits_tdi: bits.to_vec(),
+            capture: false,
+            tms_exit,
+        });
+    }
+    fn shift_bits_inout(&mut self, bits: &[bool], tms_exit: bool) -> Vec<bool> {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftBits {
+            bits_tdi: bits.to_vec(),
+            capture: true,
+            tms_exit,
+        });
+
+        let mut ret = self.flush();
+        let retlen = ret.len();
+        if let JTAGOutput::CapturedBits(out) = &mut ret[retlen - 1] {
+            out.split_off(0)
+        } else {
+            unreachable!()
+        }
+    }
+
+    fn shift_ir_out(&mut self, ir: &[bool], pause: bool) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftIR {
+            ir: ir.to_vec(),
+            capture: false,
+            pause,
+        });
+    }
+    fn shift_ir_inout(&mut self, ir: &[bool], pause: bool) -> Vec<bool> {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftIR {
+            ir: ir.to_vec(),
+            capture: true,
+            pause,
+        });
+
+        let mut ret = self.flush();
+        let retlen = ret.len();
+        if let JTAGOutput::CapturedBits(out) = &mut ret[retlen - 1] {
+            out.split_off(0)
+        } else {
+            unreachable!()
+        }
+    }
+    fn shift_dr_out(&mut self, dr: &[bool], pause: bool) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftDR {
+            dr: dr.to_vec(),
+            capture: true,
+            pause,
+        });
+    }
+    fn shift_dr_inout(&mut self, dr: &[bool], pause: bool) -> Vec<bool> {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ShiftDR {
+            dr: dr.to_vec(),
+            capture: true,
+            pause,
+        });
+
+        let mut ret = self.flush();
+        let retlen = ret.len();
+        if let JTAGOutput::CapturedBits(out) = &mut ret[retlen - 1] {
+            out.split_off(0)
+        } else {
+            unreachable!()
+        }
+    }
+
+    fn set_ir(&mut self, ir: &[bool]) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::SetIR(ir.to_vec()));
+    }
+    fn read_reg(&mut self, ir: &[bool]) -> Vec<bool> {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::ReadReg(ir.to_vec()));
+
+        let mut ret = self.flush();
+        let retlen = ret.len();
+        if let JTAGOutput::CapturedBits(out) = &mut ret[retlen - 1] {
+            out.split_off(0)
+        } else {
+            unreachable!()
+        }
+    }
+    fn write_reg(&mut self, ir: &[bool], dr: &[bool]) {
+        let state: &mut JTAGAdapterState = self.as_mut();
+        state.queued_actions.push(JTAGAction::WriteReg {
+            ir: ir.to_vec(),
+            dr: dr.to_vec(),
+        });
+    }
+}
+
+pub trait BitbangJTAGAdapter {
+    fn shift_one_bit(&mut self, tms: bool, tdi: bool) -> bool;
+}
+
+pub trait ChunkShifterJTAGAdapter {
+    fn shift_tms_chunk(&mut self, tms_chunk: &[bool]);
+    fn shift_tdi_chunk(&mut self, tdi_chunk: &[bool], tms_exit: bool);
+    fn shift_tditdo_chunk(&mut self, tdi_chunk: &[bool], tms_exit: bool) -> Vec<bool>;
+}
+
+pub trait StateTrackingJTAGAdapter {
+    fn execute_stjtag_action(&mut self, action: JTAGAction) -> JTAGOutput;
+}
+
+impl<T: StateTrackingJTAGAdapter + AsMut<JTAGAdapterState>> JTAGAdapter for T {
+    fn execute_actions(&mut self, _actions: &[JTAGAction]) -> Vec<JTAGOutput> {
+        todo!()
+    }
+}
+
+impl<T: ChunkShifterJTAGAdapter> StateTrackingJTAGAdapter for T {
+    fn execute_stjtag_action(&mut self, _action: JTAGAction) -> JTAGOutput {
+        todo!()
+    }
+}
+
+impl<T: BitbangJTAGAdapter> ChunkShifterJTAGAdapter for T {
+    fn shift_tms_chunk(&mut self, _tms_chunk: &[bool]) {
+        todo!()
+    }
+    fn shift_tdi_chunk(&mut self, _tdi_chunk: &[bool], _tms_exit: bool) {
+        todo!()
+    }
+    fn shift_tditdo_chunk(&mut self, _tdi_chunk: &[bool], _tms_exit: bool) -> Vec<bool> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,136 @@
+use crate::*;
+
+struct TestNativeJTAGAdapter<'a> {
+    jtag_state: JTAGAdapterState,
+    test_actions: &'a [JTAGAction],
+}
+impl<'a> AsMut<JTAGAdapterState> for TestNativeJTAGAdapter<'a> {
+    fn as_mut(&mut self) -> &mut JTAGAdapterState {
+        &mut self.jtag_state
+    }
+}
+impl<'a> TestNativeJTAGAdapter<'a> {
+    fn new(test_actions: &'a [JTAGAction]) -> Self {
+        Self {
+            jtag_state: JTAGAdapterState::new(),
+            test_actions,
+        }
+    }
+}
+
+impl<'a> JTAGAdapter for TestNativeJTAGAdapter<'a> {
+    fn execute_actions(&mut self, actions: &[JTAGAction]) -> Vec<JTAGOutput> {
+        assert_eq!(actions, self.test_actions);
+        vec![JTAGOutput::NoData; actions.len()]
+    }
+}
+
+struct TestBitbangJTAGAdapter {
+    jtag_state: JTAGAdapterState,
+    // bitbang_state: BitbangAdapterState,
+}
+impl AsMut<JTAGAdapterState> for TestBitbangJTAGAdapter {
+    fn as_mut(&mut self) -> &mut JTAGAdapterState {
+        &mut self.jtag_state
+    }
+}
+impl TestBitbangJTAGAdapter {
+    fn new() -> Self {
+        Self {
+            jtag_state: JTAGAdapterState::new(),
+        }
+    }
+}
+
+impl BitbangJTAGAdapter for TestBitbangJTAGAdapter {
+    fn shift_one_bit(&mut self, _tms: bool, _tdi: bool) -> bool {
+        todo!()
+    }
+}
+
+struct TestChunkJTAGAdapter {
+    jtag_state: JTAGAdapterState,
+    // bitbang_state: BitbangAdapterState,
+}
+impl AsMut<JTAGAdapterState> for TestChunkJTAGAdapter {
+    fn as_mut(&mut self) -> &mut JTAGAdapterState {
+        &mut self.jtag_state
+    }
+}
+impl TestChunkJTAGAdapter {
+    fn new() -> Self {
+        Self {
+            jtag_state: JTAGAdapterState::new(),
+        }
+    }
+}
+
+impl ChunkShifterJTAGAdapter for TestChunkJTAGAdapter {
+    fn shift_tms_chunk(&mut self, _tms_chunk: &[bool]) {
+        todo!()
+    }
+    fn shift_tdi_chunk(&mut self, _tdi_chunk: &[bool], _tms_exit: bool) {
+        todo!()
+    }
+    fn shift_tditdo_chunk(&mut self, _tdi_chunk: &[bool], _tms_exit: bool) -> Vec<bool> {
+        todo!()
+    }
+}
+
+struct TestStateJTAGAdapter {
+    jtag_state: JTAGAdapterState,
+    // bitbang_state: BitbangAdapterState,
+}
+impl AsMut<JTAGAdapterState> for TestStateJTAGAdapter {
+    fn as_mut(&mut self) -> &mut JTAGAdapterState {
+        &mut self.jtag_state
+    }
+}
+impl TestStateJTAGAdapter {
+    fn new() -> Self {
+        Self {
+            jtag_state: JTAGAdapterState::new(),
+        }
+    }
+}
+
+impl StateTrackingJTAGAdapter for TestStateJTAGAdapter {
+    fn execute_stjtag_action(&mut self, _action: JTAGAction) -> JTAGOutput {
+        todo!()
+    }
+}
+
+#[test]
+fn compile_test_trait_obj_safe() {
+    let _: &dyn JTAGAdapter;
+}
+
+#[test]
+fn test_native_adapter_fns() {
+    let mut testadapter = TestNativeJTAGAdapter::new(&[]);
+
+    testadapter.test_actions = &[JTAGAction::ResetToTLR];
+    testadapter.reset_to_tlr();
+    testadapter.flush();
+}
+
+#[test]
+fn test_bitbang_adapter() {
+    let mut testadapter = TestBitbangJTAGAdapter::new();
+
+    testadapter.reset_to_tlr();
+}
+
+#[test]
+fn test_chunked_adapter() {
+    let mut testadapter = TestChunkJTAGAdapter::new();
+
+    testadapter.reset_to_tlr();
+}
+
+#[test]
+fn test_state_tracking_adapter() {
+    let mut testadapter = TestStateJTAGAdapter::new();
+
+    testadapter.reset_to_tlr();
+}
