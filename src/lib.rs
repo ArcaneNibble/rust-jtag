b8c86dd92d717ce7f222ca63dab94a4ce981f231
R <rqou@berkeley.edu>
2022-01-29 20:47:13 -0800

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -295,130 +295,138 @@ pub trait StateTrackingJTAGAdapter {
 
 impl<T: StateTrackingJTAGAdapter + AsMut<JTAGAdapterState>> JTAGAdapter for T {
     fn execute_actions(&mut self, actions: &[JTAGAction]) -> Vec<JTAGOutput> {
-        actions.into_iter().map(|action| {
-            match action {
-                JTAGAction::DelayNS(..) |
-                JTAGAction::SetClkSpeed(..) |
-                JTAGAction::ResetToTLR |
-                JTAGAction::GoViaStates(..) |
-                JTAGAction::ShiftBits{..} => {
-                    self.execute_stjtag_action(action)
-                }
+        actions
+            .into_iter()
+            .map(|action| {
+                match action {
+                    JTAGAction::DelayNS(..)
+                    | JTAGAction::SetClkSpeed(..)
+                    | JTAGAction::ResetToTLR
+                    | JTAGAction::GoViaStates(..)
+                    | JTAGAction::ShiftBits { .. } => self.execute_stjtag_action(action),
 
-                JTAGAction::ShiftIR{ir, capture, pause} => {
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftIR]));
+                    JTAGAction::ShiftIR { ir, capture, pause } => {
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftIR,
+                        ]));
 
-                    let ret = self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        let ret = self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: ir.clone(),
                             capture: *capture,
                             tms_exit: true,
                         });
 
-                    if *pause {
-                        self.execute_stjtag_action(
-                            &JTAGAction::GoViaStates(vec![JTAGState::PauseIR]));
-                    } else {
-                        self.execute_stjtag_action(
-                            &JTAGAction::GoViaStates(vec![JTAGState::RunTestIdle]));
+                        if *pause {
+                            self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                                JTAGState::PauseIR,
+                            ]));
+                        } else {
+                            self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                                JTAGState::RunTestIdle,
+                            ]));
+                        }
+
+                        ret
                     }
+                    JTAGAction::ShiftDR { dr, capture, pause } => {
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftDR,
+                        ]));
 
-                    ret
-                }
-                JTAGAction::ShiftDR{dr, capture, pause} => {
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftDR]));
-
-                    let ret = self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        let ret = self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: dr.clone(),
                             capture: *capture,
                             tms_exit: true,
                         });
 
-                    if *pause {
-                        self.execute_stjtag_action(
-                            &JTAGAction::GoViaStates(vec![JTAGState::PauseDR]));
-                    } else {
-                        self.execute_stjtag_action(
-                            &JTAGAction::GoViaStates(vec![JTAGState::RunTestIdle]));
+                        if *pause {
+                            self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                                JTAGState::PauseDR,
+                            ]));
+                        } else {
+                            self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                                JTAGState::RunTestIdle,
+                            ]));
+                        }
+
+                        ret
                     }
 
-                    ret
-                }
+                    // XXX need to track stuff for optimization here
+                    JTAGAction::SetIR(ir) => {
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftIR,
+                        ]));
 
-                // XXX need to track stuff for optimization here
-                JTAGAction::SetIR(ir) => {
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftIR]));
-
-                    self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: ir.clone(),
                             capture: false,
                             tms_exit: true,
                         });
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::RunTestIdle]));
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::RunTestIdle,
+                        ]));
 
-                    JTAGOutput::NoData
-                }
-                JTAGAction::ReadReg{ir, drlen} => {
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftIR]));
+                        JTAGOutput::NoData
+                    }
+                    JTAGAction::ReadReg { ir, drlen } => {
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftIR,
+                        ]));
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: ir.clone(),
                             capture: false,
                             tms_exit: true,
                         });
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftDR]));
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftDR,
+                        ]));
 
-                    let ret = self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        let ret = self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: vec![false; *drlen],
                             capture: true,
                             tms_exit: true,
                         });
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::RunTestIdle]));
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::RunTestIdle,
+                        ]));
 
-                    ret
-                }
-                JTAGAction::WriteReg{ir, dr} => {
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftIR]));
+                        ret
+                    }
+                    JTAGAction::WriteReg { ir, dr } => {
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftIR,
+                        ]));
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: ir.clone(),
                             capture: false,
                             tms_exit: true,
                         });
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::ShiftDR]));
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::ShiftDR,
+                        ]));
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::ShiftBits {
+                        self.execute_stjtag_action(&JTAGAction::ShiftBits {
                             bits_tdi: dr.clone(),
                             capture: false,
                             tms_exit: true,
                         });
 
-                    self.execute_stjtag_action(
-                        &JTAGAction::GoViaStates(vec![JTAGState::RunTestIdle]));
+                        self.execute_stjtag_action(&JTAGAction::GoViaStates(vec![
+                            JTAGState::RunTestIdle,
+                        ]));
 
-                    JTAGOutput::NoData
+                        JTAGOutput::NoData
+                    }
                 }
-            }
-        }).collect()
+            })
+            .collect()
     }
 }
 
